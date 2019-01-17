af8fd8395ebb6520b8e8ece000e6d416fc1db361
alejandrohn <alehn96@gmail.com>
2019-01-17 11:38:53 +0100

    install node-rest-client

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ var T = new Twitter(config);
 var tweet;
 
 const requestHandler = (req, resp) => {
-  T.get('statuses/home_timeline', function(err, data, response) {
-	  if(!err){
-	  	tweet = data[0].text;
-	  } else {
-	    tweet = "Error al buscar tweet";
+	T.get('statuses/home_timeline', function(err, data, response) {
+		if (!err) {
+			tweet = data[0].text;
+		} else {
+			tweet = 'Error al buscar tweet';
 		}
-		console.log("Responded");
-	  resp.end(tweet);
+		console.log('Responded');
+		resp.end(tweet);
 	});
-}
+};
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
-  console.log(`server is listening on ${port}`);
-})
+	if (err) {
+		return console.log('something bad happened', err);
+	}
+	console.log(`server is listening on ${port}`);
+});
